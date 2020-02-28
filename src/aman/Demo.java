f09b2698d4ffc3b830a0ebe24781ff2206f82974
Shreyansh Jain <shreyansh791@yahoo.in>
2020-02-28 22:42:19 +0530

    1

--- src/aman/Demo.java
+++ src/aman/Demo.java
@@ -3,15 +3,8 @@ package aman;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hi");
-		System.out.println("Bye");
+		System.out.println("hello");
 
-		System.out.println("Bye123");
-
-		System.out.println("Last Line...");
-
-		System.out.println("Last Line...");
-
-		System.out.println("Last Line123456789...");
 
 	}
 }
