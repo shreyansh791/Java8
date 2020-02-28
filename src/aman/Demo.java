55998253d8167ed983470ba86c878e2ad68f6963
Shreyansh Jain <shreyansh791@yahoo.in>
2020-02-28 22:58:05 +0530

    89

--- src/aman/Demo.java
+++ src/aman/Demo.java
@@ -3,10 +3,7 @@ package aman;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hi");
-		System.out.println("Hi 1");
-		System.out.println("Hi 2");
-		System.out.println("Hi 3");
-		System.out.println("Hi 4");
+
 
 
 
