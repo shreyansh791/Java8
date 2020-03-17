d44bce98e2b4cf313ab14ed5d55afc2ed5ed3639
Shreyansh Jain <shreyansh791@yahoo.in>
2020-03-17 22:23:54 +0530

    njknk

--- src/dateandtimeapi/Demo1.java
+++ src/dateandtimeapi/Demo1.java
@@ -1,6 +1,6 @@
 package dateandtimeapi;
-
 public class Demo1 {
+
 	static int i = 10;
 	static int j;
 	static {   System.out.println("Static block initialized.");   j = i * 2; 
@@ -9,9 +9,9 @@ public class Demo1 {
 
 		System.out.println("Static Block...");
 	}
+
 	public static void main(String[] args) {  
-		System.out.println("Value of i : " + i); 
+		System.out.println("Value of i : " + i);
 		System.out.println("Value of j : " + j);
 	}
-
 }
